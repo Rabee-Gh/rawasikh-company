0a5d60bc88a8cde9084259fdb9baf0abb43a278f
Rabee-Gh <rabeeghanem0@gmail.com>
2026-05-29 15:26:10 +0300

    fix: add SPA routing support 2

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { writeFileSync } from 'fs'
-import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'copy-redirects',
-      closeBundle() {
-        writeFileSync(resolve(__dirname, 'dist/_redirects'), '/*    /index.html   200\n');
-      },
-    },
-  ],
-  base: '/',
+  plugins: [react()],
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
